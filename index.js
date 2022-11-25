891233789f9ffe0539e35ac25f8fea12b7aba66b
HugoA <aragones.hugo@gmail.com>
2022-11-25 12:12:12 +0100

    all done but nightmode

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// const dotenv = require('dotenv').config()
-
 // Constantes de la page HTML
-const resultDisplay = document.getElementsByClassName('weather-result')[0]
+const weatherDisplay = document.getElementsByClassName('weather-display')
+const weatherImage = document.getElementsByClassName('weather-image')
+const weatherText = document.getElementsByClassName('weather-main')
+const weatherDay = document.getElementsByClassName('weather-day')
 const submitBtn = document.getElementsByTagName('button')[0]
 const inputField = document.getElementsByTagName('input')[0]
+const selectField = document.getElementsByTagName('select')[0]
 
 /* -------------------- OpenCage and OpenWeather API Request -------------------- */
 
 /* ----- Constantes des API's ----- */
 // OpenCage
-const cage_key = process.env.OPENCAGE_API_KEY
+const cage_key = "0cf8ff533c404738a82298f6a2a013db" //process.env.OPENCAGE_API_KEY
 const cage_api_url = 'https://api.opencagedata.com/geocode/v1/json'
 // OpenWeather
-const weather_key = process.env.OPENWEATHER_API_KEY
-const weather_api_url = 'https://api.openweathermap.org/data/3.0/onecall'
+const weather_key = "d0fcc00c02efe5b8355fa57156f79f2b" //process.env.OPENWEATHER_API_KEY
+const weather_api_url = 'https://api.openweathermap.org/data/2.5/onecall'
+
+const imageDisplay = `<img alt="ceci est la météo" />`
 
 /* ----- Déclaration des variables ----- */
 let inputValue
@@ -22,10 +26,17 @@ let cage_request_url
 let lat
 let lon
 let weather_request_url
+let daysSelected = selectField.value
+
 
 /* ----- Fonctions / Fetch / Et autres scripts ----- */
 
-// Fonction de mise à jour des valeurs input/city/request_url
+//Mise à jour du nombre de jours à display
+selectField.onchange = () => {
+    daysSelected = selectField.value
+}
+
+//Mise à jour de la première URL
 function getValue() {
     inputValue = inputField.value
     city = inputValue
@@ -36,72 +47,72 @@ function getValue() {
         + '&pretty=1'
         + '&no_annotations=1'
 }
-
-// Aller chercher lat et lon (onChange)
-inputField.onchange = () => {
-    getValue()
-    fetch(cage_request_url)
-        .then(response => response.json())
-        .then(json => {
-            resultDisplay.innerText = `lat: ${json.results[0].geometry.lat} long: ${json.results[0].geometry.lng}`
-            lat = json.results[0].geometry.lat
-            lon = json.results[0].geometry.lng
-            weather_request_url = weather_api_url
-                + '?'
-                + 'lat=' + lat
-                + '&lon=' + lon
-                + '&exclude=hourly,daily'
-                + '&appid=' + weather_key
-            console.log(weather_request_url)
-        })
-        .catch(err => console.error('Problème avec l\'opération fetch: ' + err.message))
-}
-
-// Aller chercher la météo (onSubmit)
-submitBtn.onclick = (e) => {
-    e.preventDefault()
-    console.log(weather_request_url)
-    fetch(weather_request_url)
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('Problème avec l\'opération fetch: ' + err.message))
-}
-
-
-/* 
-
-
 inputField.oninput = () => {
     getValue()
 }
 
-//Fonction fetch de fetch
+//Première promise qui retourne les coordonées
 const getCoord = () => {
-    fetch(cage_request_url)
-        .then(resp => resp.json())
-        .then(json => {
-            lat = json.results[0].geometry.lat
-            lon = json.results[0].geometry.lng
-            weather_request_url = weather_api_url
-                + '?'
-                + 'lat=' + lat
-                + '&lon=' + lon
-                + '&exclude=hourly,daily'
-                + '&appid=' + weather_key
-        })
-        .catch(err => console.error('Problème avec l\'opération fetch: ' + err.message))
+    return fetch(cage_request_url)
+            .then(resp => resp.json())
+            .then(json => {return json.results[0].geometry})
+            .catch(err => {
+                console.error('Problème avec l\'opération fetch: ' + err.message)
+                alert('Woops! Please check your spelling.')
+            })
 }
-const getWeather = () => {
-    fetch(weather_request_url)
-        .then(resp => resp.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('Problème avec l\'opération fetch: ' + err.message))
+//Deuxième promise qui utilisera la première pour retourner la météo
+const getWeather = (arg) => {
+    weather_request_url = weather_api_url //construction de l'URL
+        + '?'
+        + 'lat=' + arg.lat
+        + '&lon=' + arg.lng
+        + '&exclude=hourly,minutely,current,alerts'
+        + '&appid=' + weather_key
+    return fetch(weather_request_url)
+            .then(resp => resp.json())
+            .then(json => {return json})
+            .catch(err => {
+                console.error('Problème avec l\'opération fetch: ' + err.message)
+                alert('Woops! Please check your spelling.')
+            })
+}
+
+//Convertir le dt en jour de la semaine
+const stampToDate = (timeStamp) => {
+    let a = new Date(timeStamp * 1000);
+    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let dayOfWeek = days[a.getDay()]
+return dayOfWeek
+}
+
+//Fonction async avec les deux promises et l'itération
+const asyncRequests = async () => {
+    const resOne = await getCoord()
+    const resTwo = await getWeather(resOne)
+    //on clear le display
+    for(let i=0; i<weatherDisplay.length; i++) {
+        weatherDisplay[i].classList.add('hidden')
+        let toClear = weatherDisplay[i].children
+        for(let j=0; j<=2; j++) {
+            if(j<2){
+                toClear[j].innerText = ""
+            } else {
+                toClear[j].innerHTML = ""
+            }
+        }
+    }
+    //on boucle dans le tableau daily de la rep JSON
+    for(let day=0; day<daysSelected; day++) {
+        weatherDisplay[day].classList.remove('hidden')
+        weatherDay[day].innerText = stampToDate(resTwo.daily[day].dt)
+        weatherText[day].innerText = `${resTwo.daily[day].weather[0].description}`
+        weatherImage[day].innerHTML = imageDisplay
+        weatherImage[day].lastElementChild.setAttribute('src', `http://openweathermap.org/img/wn/${resTwo.daily[day].weather[0].icon}@2x.png`)
+    }
 }
 
 submitBtn.onclick = async (e) => {
     e.preventDefault()
-    await getCoord()
-    await getWeather()
+    asyncRequests()
 }
-
- */
